d1567dd249479e48caf33ccac451edeb2657790f
mperry0305 <mperry0305@gmail.com>
2013-03-14 13:54:35 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,26 @@
+
+public super class Animal {
+
+  private Double
+		regNumber1,
+		regNumber2;
+	
+	private Char
+		gender;
+	
+	private Integer
+		month,
+		day,
+		year;
+	
+	private String
+		name;
+		
+	private ClassificationScore(){}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
