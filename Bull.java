a851ccffd7aa34d9a7286285b99c93d29f3cf5f3
mperry0305 <mperry0305@gmail.com>
2013-05-12 17:53:49 -0400

    Create Bull.java

---
+++ Bull.java
@@ -0,0 +1,50 @@
+
+public class Bull extends Animal{
+  private String fileName;
+	private Scanner inptFile,stdIn;
+		stdIn = new Scanner(System.in);
+		fileName = args[0];
+
+		
+			while(fileName.hasNext(stdIn.next())){
+				String[8] 
+					animalRegNum,
+					sireRegNum,
+					damRegNum,
+					gender,
+					dob,
+					name,
+					moReClass,
+					proData;
+
+
+			}
+		
+		
+		
+
+    
+    public Bull() {
+        
+        //super constructor
+        super();
+
+        String
+        	animalRegNum,
+        	sireRegNum,
+        	damRegNum,
+        	gender,
+        	dob,
+        	name,
+        	moreClass,
+        	proData;
+
+         
+    }
+    public String toString(){
+    	private Boolean has
+    }
+
+    
+   
+}
